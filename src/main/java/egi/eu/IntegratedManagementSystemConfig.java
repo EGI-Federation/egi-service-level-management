00c481e5c61086011f171639f01c28d451024d4e
Levente Farkas <thebe14@gmail.com>
2023-07-20 09:19:21 +0200

    Added Check-in group name for process to configuration

--- src/main/java/egi/eu/IntegratedManagementSystemConfig.java
+++ src/main/java/egi/eu/IntegratedManagementSystemConfig.java
@@ -15,7 +15,10 @@ public interface IntegratedManagementSystemConfig {
     // Users must be members of this VO to use the IMS tools
     String vo();
 
+    // Users must be members of this group to participate in SLM
+    String group();
+
     // Role names (in entitlements)
-    // e.g. urn:mace:egi.eu:group:vo.tools.egi.eu:role=slm-manager#aai.egi.eu"
+    // e.g. urn:mace:egi.eu:group:vo.tools.egi.eu:slm:role=process-manager#aai.egi.eu"
     Map<String, String> roles();
 }
